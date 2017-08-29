0f0d1540934b9eb226baad734f03ab4976cff6ee
Namdak Tonpa <maxim@synrc.com>
2017-08-29 12:29:22 +0300

    emqttd_kvs

---
+++ src/emqttd_kvs.erl
@@ -0,0 +1,12 @@
+-module(emqttd_kvs).
+-include_lib("kvs/include/metainfo.hrl").
+-include_lib("emqttd/include/emqttd.hrl").
+-compile(export_all).
+
+-record(mqtt_subproperty, {key,value}).
+
+metainfo() ->   #schema{name=kvs,tables= tables() }.
+tables()   -> [ #table{name=mqtt_subscription, fields=record_info(fields,mqtt_subscription)},
+                #table{name=mqtt_subproperty,  fields=record_info(fields,mqtt_subproperty)},
+                #table{name=mqtt_topic,  fields=record_info(fields,mqtt_topic)}
+              ].
