ad1805cf30d28e08dfa01050f700526d21b20c61
Namdak Tonpa <maxim@synrc.com>
2017-08-29 12:46:12 +0300

    kvs

--- src/emqttd_kvs.erl
+++ src/emqttd_kvs.erl
@@ -8,5 +8,5 @@
 metainfo() ->   #schema{name=kvs,tables= tables() }.
 tables()   -> [ #table{name=mqtt_subscription, fields=record_info(fields,mqtt_subscription)},
                 #table{name=mqtt_subproperty,  fields=record_info(fields,mqtt_subproperty)},
-                #table{name=mqtt_topic,  fields=record_info(fields,mqtt_topic)}
+                #table{name=mqtt_subscriber,  fields=record_info(fields, mqtt_subscriber)}
               ].
