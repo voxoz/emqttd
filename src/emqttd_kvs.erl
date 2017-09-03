b9c4b1fc0fdf9ee3b9c638f51b560c6976bac2ae
Namdak Tonpa <maxim@synrc.com>
2017-09-03 03:07:49 +0300

    format

--- src/emqttd_kvs.erl
+++ src/emqttd_kvs.erl
@@ -4,7 +4,16 @@
 -compile(export_all).
 
 metainfo() ->   #schema{name=kvs,tables= tables() }.
-tables()   -> [ #table{name=mqtt_subproperty,  fields=record_info(fields,mqtt_subproperty), copy_type=disc_copies, type=set},
-                #table{name=mqtt_subscription, fields=record_info(fields,mqtt_subscription), copy_type=disc_copies, type=bag},
-                #table{name=mqtt_subscriber,   fields=record_info(fields,mqtt_subscriber), copy_type=disc_copies, type=bag}
+
+tables()   -> [ #table{name=mqtt_subproperty,
+                       fields=record_info(fields,mqtt_subproperty),
+                       copy_type=disc_copies, type=set},
+
+                #table{name=mqtt_subscription,
+                       fields=record_info(fields,mqtt_subscription),
+                       copy_type=disc_copies, type=bag},
+
+                #table{name=mqtt_subscriber,
+                       fields=record_info(fields,mqtt_subscriber),
+                       copy_type=disc_copies, type=bag}
               ].
