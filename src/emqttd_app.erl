7daae227e500a8a91b399f455ef7c6671b4aca34
Namdak Tonpa <maxim@synrc.com>
2017-09-12 19:11:44 +0300

    mnesia first

--- src/emqttd_app.erl
+++ src/emqttd_app.erl
@@ -40,8 +40,8 @@
 start(_Type, _Args) ->
     lager:set_loglevel(lager_console_backend, error),
     print_banner(),
-    emqttd_mnesia:start(),
     kvs:join(),
+    emqttd_mnesia:start(),
     {ok, Sup} = emqttd_sup:start_link(),
     [ begin _ = erlang:apply(X,Y,Z),
             io:format("~p:~p: ~p~n",[X,Y,ok]) end
